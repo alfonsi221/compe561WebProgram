209ea21350e2a65d89e63177136f75d5e72ed085
alfonsi221 <alfonsi221@gmail.com>
2022-04-20 20:31:26 -0700

    updated tableau stuff

--- data.sql
+++ data.sql
@@ -1,15 +1,7 @@
 
-    alter table customer
-    add CustomerPassword varchar(30);
-    create table paymentOption(
-        ID int,
-        cardName varchar(255),
-        cardNumber varchar(16),
-        CVV varchar(3),
-        expirationMonth varchar(2),
-        expirationYear varchar(2),
-        CustomerID int foreign key references customer(CustomerID)
-    );
+    alter table item
+    add itemType varchar(20);
+
 
 
     
