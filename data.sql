db75086a48d7a91fc3101dc4080149bc5332a6f0
alfonsi221 <alfonsi221@gmail.com>
2022-04-21 21:16:42 -0700

    updated Tableau

--- data.sql
+++ data.sql
@@ -1,7 +1,44 @@
-
-    alter table item
-    add itemType varchar(20);
-
-
-
-    
+	insert into orderItem(orderItemID ,quantity, ItemID)
+		values(1,2,1);
+	insert into orderItem(orderItemID ,quantity, ItemID)
+		values(2,1,4);
+	insert into orderItem(orderItemID ,quantity, ItemID)
+		values(3,1,8);
+	insert into orderItem(orderItemID ,quantity, ItemID)
+		values(4,1,10);
+	insert into orderItem(orderItemID ,quantity, ItemID)
+		values(5,1,14);
+	insert into orderItem(orderItemID ,quantity, ItemID)
+		values(6,1,18);
+	insert into orderItem(orderItemID ,quantity, ItemID)
+		values(7,1,3);
+	insert into orderItem(orderItemID ,quantity, ItemID)
+		values(8,1,7);
+	insert into orderItem(orderItemID ,quantity, ItemID)
+		values(9,1,10);
+	insert into orderItem(orderItemID ,quantity, ItemID)
+		values(10,1,2);
+	insert into orderItem(orderItemID ,quantity, ItemID)
+		values(11,1,6);
+	insert into orderItem(orderItemID ,quantity, ItemID)
+		values(12,1,9);
+	insert into orderItem(orderItemID ,quantity, ItemID)
+		values(13,1,16);
+	insert into orderItem(orderItemID ,quantity, ItemID)
+		values(14,1,4);
+	insert into orderItem(orderItemID ,quantity, ItemID)
+		values(15,1,7);
+	insert into orderItem(orderItemID ,quantity, ItemID)
+		values(16,1,9);
+	insert into orderItem(orderItemID ,quantity, ItemID)
+		values(17,1,3);
+	insert into orderItem(orderItemID ,quantity, ItemID)
+		values(18,1,6);
+	insert into orderItem(orderItemID ,quantity, ItemID)
+		values(19,1,9);
+	insert into orderItem(orderItemID ,quantity, ItemID)
+		values(20,1,12);
+	insert into orderItem(orderItemID ,quantity, ItemID)
+		values(21,1,15);
+	insert into orderItem(orderItemID ,quantity, ItemID)
+		values(22,1,18);
